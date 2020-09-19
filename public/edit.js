603a0cc183e9b562c326aabe821bdd350ccb938d
yuyuan luo <luo.yuyuan@yahoo.com>
2020-09-19 09:36:08 -0400

    styling progress + title change mechanism

--- public/edit.js
+++ public/edit.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 
     $("#ctinput").hide();
     $("#ctsubmit").hide();
+    $("#ctcancel").hide();
 
     $("#undo").click(function () {
         sketchpad.undo();
@@ -23,14 +24,28 @@ $(document).ready(function () {
     $("#changetitle").click(function () {
         $("#ctinput").show();
         $("#ctsubmit").show();
+        $("#ctcancel").show();
         $("#changetitle").hide();
+        $("#title").hide();
     });
 
     //change title
     $("#ctsubmit").click(function () {
         $("#ctinput").hide();
         $("#ctsubmit").hide();
-        $("#title").text($('#ctinput').val());
+        $("#ctcancel").hide();
+        if ($('#ctinput').val().length != 0) {
+            $("#title").text($('#ctinput').val());
+        }
+        $("#title").show();
+        $("#changetitle").show();
+    });
+
+    $("#ctcancel").click(function () {
+        $("#ctinput").hide();
+        $("#ctsubmit").hide();
+        $("#ctcancel").hide();
+        $("#title").show();
         $("#changetitle").show();
     });
 
