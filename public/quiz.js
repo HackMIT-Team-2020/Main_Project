4d74f001b703672161351885edd5a60fcd4013f4
yuyuan luo <luo.yuyuan@yahoo.com>
2020-09-19 22:01:46 -0400

    grab answers lol

--- public/quiz.js
+++ public/quiz.js
@@ -1,38 +1,41 @@
-function getQueryString( field, url ) {
-	const href = url ? url : window.location.href;
-	const reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
-	const string = reg.exec(href);
-	return string ? string[1] : null;
+function getQueryString(field, url) {
+  const href = url ? url : window.location.href;
+  const reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+  const string = reg.exec(href);
+  return string ? string[1] : null;
 };
 $(document).ready(function () {
   if (getQueryString('id') != null) {
     console.log("CALLED")
     localStorage.setItem('review_id', getQueryString('id'))
+    correct = []
+    corid = 0
     axios.get('/getnote_text/' + localStorage.getItem('review_id'))
-        .then(function (response) {
-            let isblank = response.data.startsWith("[")
-            monster_tag = ''
-            for(piece of toQuiz(response.data)){
-              if(isblank){
-                if(piece.length>0)
-                  monster_tag+='<input type="text" value="'+piece+'"></input>'
-              }
-              else{
-                if(piece.length>0)
-                  monster_tag+=piece
+      .then(function (response) {
+        let isblank = response.data.startsWith("[")
+        monster_tag = ''
+        for (piece of toQuiz(response.data)) {
+          if (isblank) {
+            if (piece.length > 0) {
+              monster_tag += '<input type="text" id="' + corid + '"></input>'
+              correct.push(piece);
             }
-            isblank = !isblank
+          } else {
+            if (piece.length > 0)
+              monster_tag += piece
           }
-          monster_tag = '<h3>'+monster_tag+'</h3>'
-          $("#question").append(monster_tag)
-          console.log(response)
+          isblank = !isblank
+        }
+        monster_tag = '<h3>' + monster_tag + '</h3>'
+        $("#question").append(monster_tag)
+        console.log(response)
 
-    })
+      })
   }
 
-    //refer to the value inside a input box by $("#id").val()
+  //refer to the value inside a input box by $("#id").val()
 });
 
-function toQuiz(input){
+function toQuiz(input) {
   return input.split(/[\[\]]/)
 }
