76f29be53da1d8bebccce83347864fef8f7b35b9
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-09-20 02:19:50 -0400

    Added stats page

--- public/review.js
+++ public/review.js
@@ -1,10 +1,12 @@
 $(window).on('pageshow',function () {
 
-    const Schedule_Row = ({ url, title, time }) => `
-      <a href="${url}" class="list-group-item">
+    const Schedule_Row = ({ review_url, stats_url, title, time }) => `
+      <a href="${review_url}" class="list-group-item">
         <h2 class="list-group-item-text">${title}</h2>
         <p>${time}</p>
       </a>
+      <a href="${stats_url}" class="btn btn-info" role="button">View Stats</a>
+
     `;
     axios.get('/review_schedule?time='+Date.now()).then(function (response) {
         for (var i = 0; i < response.data.length; i++) {
@@ -14,7 +16,11 @@ $(window).on('pageshow',function () {
             const options = { weekday: 'long', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric' }
             const date_string = date.toLocaleDateString(undefined, options)
             console.log(date_string)
-            $('#schedule').prepend(Schedule_Row({url:'/quiz.html?id='+data.id, title:data.title, time:date_string}));
+            $('#schedule').prepend(Schedule_Row({
+              review_url:'/quiz.html?id='+data.id,
+              stats_url:'/stats.html?id='+data.id,
+              title:data.title,
+              time:date_string}));
         }
 
     });
