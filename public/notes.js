3970afc90c59cec56491f32f36a05b0a5602b123
yuyuan luo <luo.yuyuan@yahoo.com>
2020-09-20 02:12:55 -0400

    semiworking graph

--- public/notes.js
+++ public/notes.js
@@ -1,8 +1,11 @@
-$(window).on('pageshow',function () {
-    // inline styling:
-    // text-decoration:none; color: black;
-    ///
-    const Note_Card = ({id, title}) => `
+$(window).on('pageshow', function () {
+  // inline styling:
+  // text-decoration:none; color: black;
+  ///
+  const Note_Card = ({
+    id,
+    title
+  }) => `
         <a href="/edit.html?id=${id}" style="text-decoration:none; color: black;">
           <div class="card">
             <img class="card-img-top" src="/images/${id}.png" alt="notes preview" style="width: 200px;" "="">
@@ -12,40 +15,57 @@ $(window).on('pageshow',function () {
           </div>
         </a>
     `;
-    //Stop caching me chrome :(
-    axios.get('/getnotes?time='+Date.now()).then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-            var data = response.data[i];
-            console.log(data);
-            $('#cardstack').prepend(
-              Note_Card({id:data.id, title:data.title})
-            );
-        }
+  //Stop caching me chrome :(
+  axios.get('/getnotes?time=' + Date.now()).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+      var data = response.data[i];
+      console.log(data);
+      $('#cardstack').prepend(
+        Note_Card({
+          id: data.id,
+          title: data.title
+        })
+      );
+    }
 
-    });
+  });
 
 
-    const Schedule_Row = ({ url, title, time }) => `
+  const Schedule_Row = ({
+    url,
+    title,
+    time
+  }) => `
       <a href="${url}" class="list-group-item">
         <h2 class="list-group-item-text">${title}</h2>
         <p>${time}</p>
       </a>
     `;
-    axios.get('/review_schedule?time='+Date.now()).then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-            var data = response.data[i];
-            console.log(data);
-            const date = new Date(data.review_time)
-            const options = { weekday: 'long', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric' }
-            const date_string = date.toLocaleDateString(undefined, options)
-            console.log(date_string)
-            $('#schedule').prepend(Schedule_Row({url:'/quiz.html?id='+data.id, title:data.title, time:date_string}));
-        }
+  axios.get('/review_schedule?time=' + Date.now()).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+      var data = response.data[i];
+      console.log(data);
+      const date = new Date(data.review_time)
+      const options = {
+        weekday: 'long',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      }
+      const date_string = date.toLocaleDateString(undefined, options)
+      console.log(date_string)
+      $('#schedule').prepend(Schedule_Row({
+        url: '/quiz.html?id=' + data.id,
+        title: data.title,
+        time: date_string
+      }));
+    }
 
-    });
+  });
 
-    $("#newnote").click(function () {
-        localStorage.setItem('openid', '');
-        window.location.href = "/edit.html";
-    });
+  $("#newnote").click(function () {
+    localStorage.setItem('openid', '');
+    window.location.href = "/edit.html";
+  });
 });
--- public/stats.js
+++ public/stats.js
@@ -1,39 +1,34 @@
 $(document).ready(function () {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+    id = "b445a492b669686ddaf5a1e7eba70017f6d682cf";
+    axios.get('/getnote/review_data/' + id).then(function (response) {
+        console.log(response.data);
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: response.data.times,
+                datasets: [{
+                    label: 'score percentage',
+                    data: response.data.scores,
+                    backgroundColor: [
+                        'rgba(193, 124, 116, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(193, 124, 116, 1)'
+                    ],
+                    borderWidth: 1
                 }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
             }
-        }
+        });
     });
+
 })
