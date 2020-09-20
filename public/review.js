8d97a66e3bd6fa1b1997ebeb099b91da1436dc42
yuyuan luo <luo.yuyuan@yahoo.com>
2020-09-20 02:36:25 -0400

    no blue

--- public/review.js
+++ public/review.js
@@ -1,28 +1,40 @@
-$(window).on('pageshow',function () {
+$(window).on('pageshow', function () {
 
-    const Schedule_Row = ({ review_url, stats_url, title, time }) => `
-      <a href="${review_url}" class="list-group-item">
+  const Schedule_Row = ({
+    review_url,
+    stats_url,
+    title,
+    time
+  }) => `
+      <a href="${review_url}" class="list-group-item"  style="text-decoration:none; color: black;">
         <h2 class="list-group-item-text">${title}</h2>
         <p>${time}</p>
       </a>
       <a href="${stats_url}" class="btn btn-navy" role="button">View Stats</a>
 
     `;
-    axios.get('/review_schedule?time='+Date.now()).then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-            var data = response.data[i];
-            console.log(data);
-            const date = new Date(data.review_time)
-            const options = { weekday: 'long', month: 'short', day: 'numeric', hour:'numeric', minute:'numeric' }
-            const date_string = date.toLocaleDateString(undefined, options)
-            console.log(date_string)
-            $('#schedule').prepend(Schedule_Row({
-              review_url:'/quiz.html?id='+data.id,
-              stats_url:'/stats.html?id='+data.id,
-              title:data.title,
-              time:date_string}));
-        }
+  axios.get('/review_schedule?time=' + Date.now()).then(function (response) {
+    for (var i = 0; i < response.data.length; i++) {
+      var data = response.data[i];
+      console.log(data);
+      const date = new Date(data.review_time)
+      const options = {
+        weekday: 'long',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      }
+      const date_string = date.toLocaleDateString(undefined, options)
+      console.log(date_string)
+      $('#schedule').prepend(Schedule_Row({
+        review_url: '/quiz.html?id=' + data.id,
+        stats_url: '/stats.html?id=' + data.id,
+        title: data.title,
+        time: date_string
+      }));
+    }
 
-    });
+  });
 
 });
