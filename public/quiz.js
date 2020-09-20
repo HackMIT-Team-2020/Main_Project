2e0c0b6018297cee2e76b6d0a96c897bd45d8e04
yuyuan luo <luo.yuyuan@yahoo.com>
2020-09-19 21:58:10 -0400

    Revert "grab answers into array"
    This reverts commit b7a46d5bfbfd18263ceff6545dd25d1b6795b345.

--- public/quiz.js
+++ public/quiz.js
@@ -1,41 +1,38 @@
-function getQueryString(field, url) {
-  const href = url ? url : window.location.href;
-  const reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
-  const string = reg.exec(href);
-  return string ? string[1] : null;
+function getQueryString( field, url ) {
+	const href = url ? url : window.location.href;
+	const reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+	const string = reg.exec(href);
+	return string ? string[1] : null;
 };
 $(document).ready(function () {
   if (getQueryString('id') != null) {
     console.log("CALLED")
     localStorage.setItem('review_id', getQueryString('id'))
     axios.get('/getnote_text/' + localStorage.getItem('review_id'))
-      .then(function (response) {
-        let isblank = response.data.startsWith("[")
-        correct = []
-        corid = 0
-        console.log(isblank)
-        for (piece of toQuiz(response.data)) {
-          if (isblank) {
-            if (piece.length > 0) {
-              $("#question").append('<input type="text" id="' + 0 + '"></input>')
-              correct.push(piece.slice(1, piece.length - 1));
+        .then(function (response) {
+            let isblank = response.data.startsWith("[")
+            console.log(isblank)
+            for(piece of toQuiz(response.data)){
+              if(isblank){
+                if(piece.length>0)
+                $("#question").append('<input type="text" value="'+piece+'"></input>')
+              }
+              else{
+                if(piece.length>0){
+                  $("#question").append('<h3>'+piece+'<h3/>')
+                  console.log("PIECE "+piece)
+                }
             }
-          } else {
-            if (piece.length > 0) {
-              $("#question").append('<h3>' + piece + '<h3/>')
-              console.log("PIECE " + piece)
-            }
+            isblank = !isblank
           }
-          isblank = !isblank
-        }
-        console.log(response)
+          console.log(response)
 
-      })
+    })
   }
 
-  //refer to the value inside a input box by $("#id").val()
+    //refer to the value inside a input box by $("#id").val()
 });
 
-function toQuiz(input) {
+function toQuiz(input){
   return input.split(/[\[\]]/)
 }
